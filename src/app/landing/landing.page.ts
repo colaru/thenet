1d84a33dcfda3ec2770ecc6da9c1f057559b92f6
colaru <colaru@gmail.com>
2018-08-03 11:52:56 +0300

    Generated landing page

---
+++ src/app/landing/landing.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'landing',
+  templateUrl: './landing.page.html',
+  styleUrls: ['./landing.page.scss'],
+})
+export class LandingPage implements OnInit {
+
+  
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
