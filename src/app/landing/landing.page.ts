8427b7047799eb3cd8b792622c2d74f3740ee5df
colaru <colaru@gmail.com>
2018-08-08 22:44:49 +0300

    Chapter about PWA

--- src/app/landing/landing.page.ts
+++ src/app/landing/landing.page.ts
@@ -35,13 +35,14 @@ export class LandingPage implements OnInit {
 
 
         // FirebaseUI config.
+
         var uiConfig = {
             signInSuccessUrl: '/users-cards',
             signInOptions: [
                 // Leave the lines as is for the providers you want to offer your users.
                 firebase.auth.GoogleAuthProvider.PROVIDER_ID,
                 firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+                // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
                 // firebase.auth.GithubAuthProvider.PROVIDER_ID,
                 firebase.auth.EmailAuthProvider.PROVIDER_ID,
                 firebase.auth.PhoneAuthProvider.PROVIDER_ID,
